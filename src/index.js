40ee6b798438df77a8e8def6528735e340e055c5
愚道 <tingzhao.ytz@antfin.com>
2018-12-11 20:14:40 +0800

    feat: support mock umi-request

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // ref:
 // - https://umijs.org/plugin/develop.html
 import { join, relative } from 'path';
+import { existsSync } from 'fs';
 import assert from 'assert';
 
 process.env.PAGES_PATH = 'src';
@@ -9,7 +10,8 @@ const layouts = ['ant-design-pro'];
 
 export default function (api, options = {}) {
   const { paths } = api;
-  const path = process.env.BLOCK_PATH || options.path || '/';
+  const path = process.env.BLOCK_DEV_PATH || options.path || '/';
+  const mockUmiRequest = process.env.BLOCK_DEV_MOCK_UMI_REQUEST === 'true' || options.mockUmiRequest || false;
 
   api.modifyDefaultConfig(memo => {
     if (options.layout) {
@@ -39,6 +41,21 @@ export default function (api, options = {}) {
       }],
     };
   });
+
+  if (mockUmiRequest && existsSync(join(paths.absPagesPath, '_mock.js'))) {
+    // build mock data to dist, for static block demo
+    api.addEntryImportAhead({
+      source: join(paths.absPagesPath, '_mock.js'),
+      specifier: '__block_mock',
+    });
+    api.addEntryCodeAhead(`
+      window.g_block_mock = __block_mock;
+    `);
+    api.chainWebpackConfig(webpackConfig => {
+      webpackConfig.resolve.alias
+        .set('umi-request$', join(__dirname, 'mock-request.js'))
+    });
+  }
 
   api.chainWebpackConfig(webpackConfig => {
     webpackConfig.resolve.alias
