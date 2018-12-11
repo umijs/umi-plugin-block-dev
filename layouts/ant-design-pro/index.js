b105daf1a47a192596923778afcf9d0df2ca89b4
愚道 <tingzhao.ytz@antfin.com>
2018-12-11 11:44:35 +0800

    use global less

--- layouts/ant-design-pro/index.js
+++ layouts/ant-design-pro/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import styles from './style.less';
 import logo from './logo.svg';
+import './style.less';
 
 export default (props) => {
   const { children } = props;
