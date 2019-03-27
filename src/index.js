6ed7d127fcbf313c50ddf6a12e610c13a9ac58bc
陈帅 <qixian.cs@outlook.com>
2019-03-27 12:35:05 +0800

    version: up to 1.3.1

--- src/index.js
+++ src/index.js
@@ -1,64 +1,78 @@
 // ref:
 // - https://umijs.org/plugin/develop.html
-import { join, relative } from 'path';
-import { existsSync } from 'fs';
-import assert from 'assert';
+import { join, relative } from "path";
+import { existsSync } from "fs";
+import assert from "assert";
 
-process.env.PAGES_PATH = 'src';
+if (!process.env.PAGES_PATH) {
+  process.env.PAGES_PATH = "src";
+}
 
-const layouts = ['ant-design-pro', 'ant-design-pro-user'];
+const layouts = ["ant-design-pro", "ant-design-pro-user"];
 
-export default function (api, options = {}) {
+export default function(api, options = {}) {
   const { paths } = api;
-  const path = process.env.BLOCK_DEV_PATH || options.path || '/';
-  const mockUmiRequest = process.env.BLOCK_DEV_MOCK_UMI_REQUEST === 'true' || options.mockUmiRequest || false;
-
+  const path = process.env.BLOCK_DEV_PATH || options.path || "/";
+  const mockUmiRequest =
+    process.env.BLOCK_DEV_MOCK_UMI_REQUEST === "true" ||
+    options.mockUmiRequest ||
+    false;
   api.modifyDefaultConfig(memo => {
     if (options.layout) {
-      assert(layouts.includes(options.layout), `layout must be one of ${layouts.join(',')}`)
+      assert(
+        layouts.includes(options.layout),
+        `layout must be one of ${layouts.join(",")}`
+      );
       const layout = join(__dirname, `../layouts/${options.layout}`);
       const pathToLayout = relative(paths.absPagesPath, layout);
       return {
         ...memo,
-        routes: [{
-          path: '/',
-          component: pathToLayout,
-          routes: [{
-            path,
-            component: './',
-            exact: false,
-          }],
-        }],
-        extraBabelIncludes: [layout],
+        routes: [
+          {
+            path: "/",
+            component: pathToLayout,
+            routes: [
+              {
+                path,
+                component: "./",
+                exact: false
+              }
+            ]
+          }
+        ],
+        extraBabelIncludes: [layout]
       };
     }
     return {
       ...memo,
-      routes: [{
-        path,
-        component: './',
-        exact: false,
-      }],
+      routes: [
+        {
+          path,
+          component: "./",
+          exact: false
+        }
+      ]
     };
   });
 
-  if (mockUmiRequest && existsSync(join(paths.absPagesPath, '_mock.js'))) {
+  if (mockUmiRequest && existsSync(join(paths.absPagesPath, "_mock.js"))) {
     // build mock data to dist, for static block demo
     api.addEntryImportAhead({
-      source: join(paths.absPagesPath, '_mock.js'),
-      specifier: '__block_mock',
+      source: join(paths.absPagesPath, "_mock.js"),
+      specifier: "__block_mock"
     });
     api.addEntryCodeAhead(`
       window.g_block_mock = __block_mock;
     `);
     api.chainWebpackConfig(webpackConfig => {
-      webpackConfig.resolve.alias
-        .set('umi-request$', join(__dirname, 'mock-request.js'))
+      webpackConfig.resolve.alias.set(
+        "umi-request$",
+        join(__dirname, "mock-request.js")
+      );
     });
   }
 
   api.chainWebpackConfig(webpackConfig => {
-    webpackConfig.resolve.alias
-      .set('@', join(paths.absSrcPath, '@'));
+    webpackConfig.resolve.alias.set("@", join(paths.absSrcPath, "@"));
   });
 }
