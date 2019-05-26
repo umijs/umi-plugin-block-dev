69f5039c48ed4a17f2f7c83cd25468a8649549d2
陈帅 <qixian.cs@outlook.com>
2019-05-26 15:18:48 +0800

    add SettingDrawer

--- layouts/ant-design-pro/index.js
+++ layouts/ant-design-pro/index.js
@@ -1,18 +1,15 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
-import { BasicLayout } from '@ant-design/pro-layout';
+import { BasicLayout, SettingDrawer } from '@ant-design/pro-layout';
 export default props => {
   const { children } = props;
   const [settings, changeSetting] = useState();
-  console.log(settings);
   return (
-    <BasicLayout
-      logo={logo}
-      settings={settings}
-      {...props}
-      onChangeSetting={settings => changeSetting(settings)}
-    >
-      {children}
-    </BasicLayout>
+    <React.Fragment>
+      <BasicLayout logo={logo} {...props} {...settings}>
+        {children}
+      </BasicLayout>
+      <SettingDrawer onSettingChange={settings => changeSetting(settings)} />
+    </React.Fragment>
   );
 };
