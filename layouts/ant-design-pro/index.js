5dc178f8f76ca6fc4e515215b87224be6bd9d39c
陈帅 <qixian.cs@outlook.com>
2019-07-16 21:52:28 +0800

    add onCollapse

--- layouts/ant-design-pro/index.js
+++ layouts/ant-design-pro/index.js
@@ -6,12 +6,15 @@ import SelectLang from './SelectLang';
 export default props => {
   const { children } = props;
   const [settings, changeSetting] = useState();
+  const [collapsed, setCollapsed] = useState(false);
   return (
     <React.Fragment>
       <BasicLayout
+        collapsed={collapsed}
         logo={logo}
         {...props}
         {...settings}
+        onCollapse={setCollapsed}
         rightContentRender={props => <SelectLang {...props} />}
       >
         {children}
